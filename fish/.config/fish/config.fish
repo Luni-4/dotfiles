8f4dfd680081e995d6a3fd2d0cfc7fa0272be2d4
Luni-4 <luni-4@hotmail.it>
2019-11-30 12:32:34 +0100

    Update fish prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,3 +14,29 @@ fish_vi_key_bindings
 
 # Use a line as cursor shape in vi insert mode
 set -g fish_cursor_insert line
+
+function fish_prompt
+
+    # Print name and hostname
+    set_color d95
+    echo -n (whoami)'@power '
+    set_color normal
+
+    # Print VCS (git)
+    set -l vcs (__fish_vcs_prompt)
+    if test -n "$vcs"
+         echo -n '('
+         set_color d9c
+         printf "%s" (string replace -r ' \((\w+)\)' '$1' (__fish_vcs_prompt))
+         set_color normal
+         echo -n ') '
+    end
+
+    # Print PWD
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+
+    echo -n '$ '
+
+end
